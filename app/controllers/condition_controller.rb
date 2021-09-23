631ec618ff9447fd9685a727ef2509530c51b562
nbur4556 <nbur4556@gmail.com>
2021-09-23 11:06:56 -0500

    feat: create index and create routes for creating and retrieving conditions

--- app/controllers/condition_controller.rb
+++ app/controllers/condition_controller.rb
@@ -1,7 +1,9 @@
 class ConditionController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def index 
         @conditions = Condition.all
-        puts @conditions
+        render json: {message: 'success', data: @conditions}
     end 
 
     def show
@@ -9,7 +11,31 @@ class ConditionController < ApplicationController
     end
 
     def create
-        # Create weather condition here
+        @condition = Condition.new(
+            date_time: '09/23/2021 10:35 AM', 
+            temp_c: '24',
+            temp_f: '74',
+            weather_code: '113',
+            icon: 'http://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png',
+            desc: 'Sunny',
+            windspeed_mph: '4',
+            windspeed_kmph: '6',
+            winddir_degree: '140',
+            winddir_point: 'SE',
+            precip_mm: '0.0',
+            precip_in: '0.0',
+            humidity: '33',
+            visibility: '16',
+            visibility_miles: '9',
+            pressure: '1022',
+            pressure_in: '30',
+            cloud_cover: '0',
+            feels_like_c: '25',
+            feels_like_f: '76',
+            uv: '1'
+        )
+
+        render json: {message: 'success', data: @condition}
     end
 
     def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get "/conditions", to: "condition#index"
+  post "/condition", to: "condition#create"
 end
