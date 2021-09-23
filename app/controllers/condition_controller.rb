dc2a148a82922c45c37458bed476507ca47fcd69
nbur4556 <nbur4556@gmail.com>
2021-09-23 12:49:25 -0500

    fix: use request body in updating conditions

--- app/controllers/condition_controller.rb
+++ app/controllers/condition_controller.rb
@@ -1,6 +1,13 @@
 class ConditionController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    def permit_params(params)
+        return params.permit(:date_time, :temp_c,:temp_f, :weather_code, :icon, :desc,
+            :windspeed_mph, :windspeed_kmph, :winddir_degree, :winddir_point, :precip_mm, :precip_in,
+            :humidity, :visibility, :visibility_miles, :pressure, :pressure_in, :cloud_cover,
+            :feels_like_c, :feels_like_f, :uv)
+    end
+
     # Get all conditions -- GET /conditions
     def index 
         @conditions = Condition.all
@@ -15,10 +22,7 @@ class ConditionController < ApplicationController
 
     # Create new condition -- POST /condition
     def create
-        api_params = params.permit(:date_time, :temp_c,:temp_f, :weather_code, :icon, :desc,
-            :windspeed_mph, :windspeed_kmph, :winddir_degree, :winddir_point, :precip_mm, :precip_in,
-            :humidity, :visibility, :visibility_miles, :pressure, :pressure_in, :cloud_cover,
-            :feels_like_c, :feels_like_f, :uv)
+        api_params = permit_params(params)
 
         @condition = Condition.new(api_params)
         @condition.save
@@ -26,7 +30,9 @@ class ConditionController < ApplicationController
     end
 
     def update
-        @condition = Condition.update(params[:id], :temp_c => '5000')
+        api_params = permit_params(params)
+
+        @condition = Condition.update(params[:id], api_params)
         render json: {message: 'success', data: @condition}
     end
 
