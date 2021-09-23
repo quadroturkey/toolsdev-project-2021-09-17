abcb91675e7c1bdcd82389247cea24a0a96d7f82
nbur4556 <nbur4556@gmail.com>
2021-09-22 21:31:58 -0500

    feat: fetch data using latitude and longitude

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,27 @@
 require 'net/http'
+require 'uri'
 
 class HomeController < ApplicationController
   def index
     ww_key = ENV['WORLD_WEATHER_KEY']
+    ww_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 
-    api_variables = {"key" => ww_key, "q" => "London", "format" => "json", "num_of_days" => "5"}
-    get_data("http://api.worldweatheronline.com/premium/weather.ashx", api_variables)
+    lat = 30.404251
+    lon = -97.849442
+
+    api_variables = {:key => ww_key, :q => "#{lat},#{lon}", :format => 'json', :num_of_days => '5'}
+    response = fetch_data(ww_url, api_variables)
+    puts response.body
   end
 
   # GET method for retrieving data from an API using uri variables
   def fetch_data(api_url, api_variables)
-    api_variables_string = ""
+    api_variables_string = "?"
     api_variables.each_key{|key| api_variables_string += "#{key}=#{api_variables[key]}&"}
-    uri = "#{api_url}?#{api_variables_string}"
+
+    puts api_variables_string
     
-    puts uri
+    response = Net::HTTP.get_response(URI(api_url + api_variables_string))
+    return response
   end 
 end
