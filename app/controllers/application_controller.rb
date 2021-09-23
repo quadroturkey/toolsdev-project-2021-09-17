5edc88cada231b7d32dc317ac2f307ee42cbffcd
nbur4556 <nbur4556@gmail.com>
2021-09-23 15:22:54 -0500

    ref: move uri and fetch methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def build_uri(api_url, variables)
+    variable_str = ''
+    variables.each_key {|key| variable_str += "#{key}=#{variables[key]}&"}
+    return URI("#{api_url}?#{variable_str}")
+  end
+
+  # Fetch data from an API using uri variables
+  def fetch_from_api(uri)    
+    response = Net::HTTP.get_response(uri)
+    return JSON.parse(response.body)
+  end 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,16 @@
-require 'net/http'
-require 'uri'
-require 'json'
-
 class HomeController < ApplicationController
+
   def index
     ww_key = ENV['WORLD_WEATHER_KEY']
     ww_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
     lat = 30.404251
     lon = -97.849442
     api_variables = {:key => ww_key, :q => "#{lat},#{lon}", :format => 'json', :num_of_days => '5'}
-  end
 
-  # GET method for retrieving data from an API using uri variables
-  def fetch_from_api(api_url, api_variables)
-    api_variables_string = "?"
-    api_variables.each_key{|key| api_variables_string += "#{key}=#{api_variables[key]}&"}
+    ww_uri = self.build_uri(ww_url, api_variables)
+    data = self.fetch_from_api(ww_uri)
 
-    puts api_variables_string
-    
-    response = Net::HTTP.get_response(URI(api_url + api_variables_string))
-    return JSON.parse(response.body)
-  end 
+    puts data
+  end  
+
 end
